95f153344a03da7dd8c35a6023a78cb43942786b
Amal Matty Antony <amalmattyantony@gmail.com>
2024-10-06 23:35:36 -0400

    fix: import initial data

--- backend/src/db/init_db.py
+++ backend/src/db/init_db.py
@@ -1,7 +1,10 @@
 import logging
 
-from src.db.session import SessionLocal
+from sqlmodel import SQLModel
 
+from src.models.position import Position
+from src.db.session import SessionLocal
+from src.db.session import engine, settings
 
 logging.basicConfig(level=logging.INFO)
 
@@ -9,19 +12,26 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def create_init_data() -> None:
-    with open("init-data.json") as f:
-        print(f.readlines())
-    with SessionLocal() as session:
+import json
 
-        session.add()
-        session.add()
 
+def create_init_data(file_name="init-data.json") -> None:
+    with open(file_name) as f:
+        positions = json.loads("".join(f.readlines()))
+        with SessionLocal() as session:
+            for position in positions:
+                position_obj = Position(**position)
+                session.merge(position_obj)
         session.commit()
+
+
+def initialize():
+    SQLModel.metadata.create_all(engine)
 
 
 def main() -> None:
     logger.info("Creating initial data")
+    initialize()
     create_init_data()
     logger.info("Initial data created")
 
